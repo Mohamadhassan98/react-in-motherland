cd846e1d19f527c47f85b9058033a00e62dc11a4
Mohamadhassan Ebrahimi <emohamadhassan@gmail.com>
2020-06-15 10:44:05 +0430

    init structure

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import Home from "./src/pages/Home";
+import getTheme from "./native-base-theme/components";
+import material from "./native-base-theme/variables/material";
+import {StyleProvider} from "native-base";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <StyleProvider style={getTheme(material as any)}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name='Home' component={Home} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </StyleProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+    },
+});
+
+export default function () {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
