68e7f8e2a28be62d0d5ff3c04ae4caf2cd6677db
Mohamadhassan Ebrahimi <emohamadhassan@gmail.com>
2020-06-15 22:14:08 +0430

    Added string internationalization

--- src/values/strings.ts
+++ src/values/strings.ts
@@ -1,7 +1,6 @@
 import i18n from "i18n-js";
 import * as Localization from "expo-localization";
 
-// export function initLocalizations() {
 i18n.translations = {
     en: {
         messenger: "messenger",
