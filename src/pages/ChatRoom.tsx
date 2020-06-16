1074ea3e901a8ef2046048747856676315ec1f34
ali <mabm13770@gmail.com>
2020-06-16 11:10:46 +0430

    syntax error in ChatRoom.tsx fixed

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body} from "native-base";
 import {t} from "i18n-js";
-import MainPageLayout from "../components/MainPageLayout";
-const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-    },
-});
-
 export default function () {
     return (
-        <MainPageLayout active={0}>
-            <View style={styles.container}>
-                <Text>{t("chat room")}</Text>
-            </View>
-        </MainPageLayout>
+        <Container></Container>
     );
 }
